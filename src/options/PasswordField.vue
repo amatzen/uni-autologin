c9638b690b9ec9dabd923bb93d0bc13bb000e5b4
Alexander Matzen <alexander@amatzen.dk>
2020-09-30 22:47:58 +0200

    Added Vue optimization 1/1
    Main task: Recode most vanilla JS to Vue.js-powered app

--- src/options/PasswordField.vue
+++ src/options/PasswordField.vue
@@ -15,6 +15,8 @@
             @mouseover="hover = true"
             @mouseleave="hover = false"
 
+            v-model="data.password"
+            @keyup="validateInput()"
         >
         <small style="display:block;">(Hold musen over feltet for at se koden)</small>
     </div>
@@ -26,7 +28,69 @@ export default {
     data() {
         return {
             hover: false,
+            data: {
+                password: ""
+            },
+            valid: {
+                passwordAintUndefined: false,
+                passwordLength: false,
+                passwordHasUppercase: false,
+                passwordHasNumber: false
+            },
+            errs: []
         }
+    },
+    methods: {
+        validateInput() {
+            this.validateLength();
+            this.validateAintUndefined();
+        },
+
+        validateLength() {
+            if( this.data.password.length < 8 ) {
+                this.valid.password = false;
+            } else {
+                this.valid.passwordLength = true;
+            }
+        },
+
+        validateAintUndefined() {
+            if( this.data.password == undefined || this.data.password == "" ) {
+                this.valid.passwordAintUndefined = false;
+            } else {
+                this.valid.passwordAintUndefined = true;
+            }
+        },
+
+        showErrors() {
+            if(!this.valid.passwordLength) {
+                this.errs.push(`Adgangskoden skal minimum være 8 tegn. Koden er lige nu ${this.data.password.length} tegn.`);
+            }
+
+            if(!this.valid.passwordAintUndefined) {
+                this.errs.push(`Adgangskoden skal udfyldes.`);
+            }
+
+            if(!this.valid.passwordHasUppercase) {
+                this.errs.push(`Adgangskoden skal minimum have ét stort bogstav.`);
+            }
+
+            if(!this.valid.passwordAintUndefined) {
+                this.errs.push(`Adgangskoden skal minimum have ét tal.`);
+            }
+        }
+    },
+    created() {
+        this.$on("trigger_validate_password", _ => {
+            this.validateInput();
+            this.showErrors();
+
+            if( errs.length == 0 ) {
+                return {err: false};
+            }
+
+            return {err: true};
+        });
     }
 }
 </script>
--- src/options/UsernameField.vue
+++ src/options/UsernameField.vue
@@ -1,12 +1,80 @@
 <template>
     <div class="form-group">
         <label class="form-label icon-link" for="username" id="usernameLabel">Brugernavn</label>
-        <input class="form-input" id="username" value="" name="username" type="text" autocomplete="off" aria-labelledby="usernameLabel" alt="Brugernavn">
-    </div>
+        <input
+            class="form-input"
+            id="username"
+            name="username"
+            type="text"
+            autocomplete="off"
+            aria-labelledby="usernameLabel"
+            alt="Brugernavn"
+
+            v-model="data.username"
+            @keyup="validateInput()"
+        >
+
+   </div>
 </template>
 
 <script>
 export default {
-    name: 'UsernameField'
+    name: 'UsernameField',
+    data() {
+        return {
+            data: {
+                username: ""
+            },
+            valid: {
+                usernameAintUndefined: false,
+                usernameLength: false
+            },
+            errs: []
+        }
+    },
+    methods: {
+        validateInput() {
+            this.validateLength();
+            this.validateAintUndefined();
+        },
+
+        validateLength() {
+            if( this.data.username.length != 8 ) {
+                this.valid.usernameLength = false;
+            } else {
+                this.valid.usernameLength = true;
+            }
+        },
+
+        validateAintUndefined() {
+            if( this.data.username == undefined || this.data.username == "" ) {
+                this.valid.usernameAintUndefined = false;
+            } else {
+                this.valid.usernameAintUndefined = true;
+            }
+        },
+
+        showErrors() {
+            if(!this.valid.usernameLength) {
+                this.errs.push(`Brugernavnet skal være 8 tegn. Brugernavnet er lige nu ${this.data.username.length} tegn.`);
+            }
+
+            if(!this.valid.usernameAintUndefined) {
+                this.errs.push(`Brugernavnet skal udfyldes.`);
+            }
+        }
+    },
+    created() {
+        this.$on("trigger_validate_username", _ => {
+            this.validateInput();
+            this.showErrors();
+
+            if( errs.length == 0 ) {
+                return {err: false};
+            }
+
+            return {err: true};
+        });
+    }
 }
 </script>
