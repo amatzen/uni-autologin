da1c2752698d44d16c1e5ffa0f83e153950c145f
Alexander Matzen <alexander@amatzen.dk>
2020-09-30 21:38:50 +0200

    Added Webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, '/src/app.js'),
+        options: path.join(__dirname, '/src/options/options.js')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ]
+}
