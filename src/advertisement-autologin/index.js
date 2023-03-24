dd13d349249fff8d66a62dc0bd14b6ab8718ba1b
jona799t <70392256+jona799t@users.noreply.github.com>
2023-03-24 17:21:29 +0100

    The extension is now working again
    Fixed the querySelector path since UniLogin updated their site.

--- src/advertisement-autologin/index.js
+++ src/advertisement-autologin/index.js
@@ -7,7 +7,7 @@ import VueApp from './App.vue'
 if(isBroker() && f1.ifElementExists("div.logo") && ( f1.ifElementExists("#username") ||  f1.ifElementExists("[type='password']") && f1.ifElementExists("body > main > div > div > form > div.row") )) {
     var e = document.createElement("div");
     e.id = "al-app";
-    document.querySelector("body > main > div > div > form > div.row").insertAdjacentElement("beforebegin", e);
+    document.querySelector("body > div.container-fluid.page-container.outer > div > div > main > form > div.row").insertAdjacentElement("beforebegin", e);
 
     new Vue({
         render: createElement => createElement(VueApp) 
