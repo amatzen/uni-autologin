1cfdb2846115d00ad7f7ddf1459f2346a5d306c3
Alexander Matzen <git@alexander.dk>
2023-05-25 16:03:46 +0000

    Simplify isBroker feature

--- src/broker.js
+++ src/broker.js
@@ -1,7 +1,7 @@
 import f1 from './f1'
 
 export default function isBroker() {
-    if (
+    return (
         (
             f1.glob('https://broker.unilogin.dk/auth*', f1.tabUrl()) ||
             f1.glob('https://idp.unilogin.dk/auth*', f1.tabUrl())
@@ -11,9 +11,5 @@ export default function isBroker() {
             !f1.glob('https://idp.unilogin.dk/auth/realms/idp/login-actions/reset-credentials*', f1.tabUrl()) &&
             !f1.glob('https://broker.unilogin.dk/auth/realms/idp/login-actions/reset-credentials*', f1.tabUrl())
         )
-    ) {
-        return true;
-    } else {
-        return false;
-    }
+    )
 }
